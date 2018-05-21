acb86ba82dbeb14dcb61a851d0cc48e9fa050a65
Emily Kolar <emkolardev@gmail.com>
2018-05-20 21:57:34 -0500

    update ignore, build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withCSS = require('@zeit/next-css');
 module.exports = withCSS({
   cssModules: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  assetPrefix: '/assets',
   webpack: (config, { defaultLoaders }) => {
     config.module.rules.push({
       test: /\.md$/,
